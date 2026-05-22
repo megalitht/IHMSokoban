87c2e786c410e0fc30725b845e007eaf6925a2a9
Antoine Meunier <antoine-meunier@MacBook-Air-de-Antoine.local>
2026-05-22 11:21:34 +0200

    modification 2 : Ajout de fonctionalité

--- src/main/java/ihm/sokoban/SokobanController.java
+++ src/main/java/ihm/sokoban/SokobanController.java
@@ -1,6 +1,8 @@
 package ihm.sokoban;
 
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 import java.util.ResourceBundle;
 
@@ -9,6 +11,7 @@ import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Alert;
 import javafx.scene.control.ButtonType;
+import javafx.scene.control.ChoiceDialog;
 import javafx.stage.Stage;
 
 
@@ -24,16 +27,58 @@ public class SokobanController implements Initializable{
 
     @FXML
     private void ButtonPlay() {
-        System.out.println("Lancement du tutoriel...");
+        System.out.println("Lancement du jeu...");
 
+        List<String> niveaux = new ArrayList<>();
+        for (int i = 1; i <= 9; i++) {
+            niveaux.add("Niveau " + i);
+        }
+
+        ChoiceDialog<String> jeu = new ChoiceDialog<>("Niveau 1", niveaux);
+        jeu.setTitle("Lancement du jeu");
+        jeu.setHeaderText("Quel niveau voulez-vous jouer ?");
+        jeu.setContentText("Sélectionnez votre niveau :");
+
+        Optional<String> reponse = jeu.showAndWait();
+
+        if (reponse.isPresent()) {
+            String niveauSelectionne = reponse.get();
+            System.out.println("Chargement du " + niveauSelectionne + "...");
+
+            switch (niveauSelectionne) {
+                case "Niveau 1":
+                    break;
+                case "Niveau 2":
+                    break;
+                case "Niveau 3":
+                    break;
+                case "Niveau 4":
+                    break;
+                case "Niveau 5":
+                    break;
+                case "Niveau 6":
+                    break;
+                case "Niveau 7":
+                    break;
+                case "Niveau 8":
+                    break;
+                case "Niveau 9":
+                    break;
+                default:
+                    break;
+            }
+        } else {
+            System.out.println("Aucun niveau sélectionné (Action annulée).");
+        }
     }
+    
     @FXML
     private void AffichageCredits() {
         System.out.println("Affichage des crédits...");
         Alert Credits = new Alert(Alert.AlertType.INFORMATION);
         Credits.setTitle("Crédits");
         Credits.setHeaderText("Crédits du projet Sokoban");
-        Credits.setContentText("Ce projet a été réalisé par Antoine Meunier dans le cadre du TP d'IHM à l'IUT de Blagnac.");
+        Credits.setContentText("Le jeu original a été créer par Hiroyuki Imabayashi en 1982 \n\nMais ce dérivé a été réalisé par Antoine Meunier dans le cadre du TP d'IHM à l'IUT de Blagnac.");
         
 
         Credits.showAndWait();
@@ -45,7 +90,7 @@ public class SokobanController implements Initializable{
         Alert Aide = new Alert(Alert.AlertType.INFORMATION);
         Aide.setTitle("Aide");
         Aide.setHeaderText("Aide pour Sokoban");
-        Aide.setContentText("Utilisez les touche Z, Q, S et D du clavier pour déplacer le personnage et poussez les caisses sur les emplacements indiqués. Le but du jeu est de déplacer toutes les caisses sur les cibles en un minimum de mouvements. Faites attention à ne pas vous retrouver coincé !\n\nSi toutefois vous trouvez un bug, veuillez contacter le réalisateur de ce projet. Pseudo Discord : megalitht");
+        Aide.setContentText("Utilisez les touche Z, Q, S et D du clavier pour déplacer le personnage et poussez les caisses sur les emplacements indiqués. Le but du jeu est de déplacer toutes les caisses sur les cibles en un minimum de mouvements. Faites attention à ne pas vous retrouver coincé !\n\nSi toutefois vous trouvez un bug, veuillez contacter le réalisateur de ce projet. \n\nPseudo Discord : megalitht");
         
         Aide.showAndWait();
     
